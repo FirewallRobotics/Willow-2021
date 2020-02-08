687383c3b5822fa36f288b22b1e2c0f0d40ef49e
Terry Phillips <terrydphillips@gmail.com>
2020-02-08 14:55:17 -0500

    Update RobotMap.java (#18)

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -23,9 +23,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * floating around.
  */
 public class RobotMap {
+    // CAN motor controllers
+    public static int LEFT_MASTER_DRIVETRAIN_ADDRESS  = 1;
+    public static int RIGHT_MASTER_DRIVETRAIN_ADDRESS = 2;
+    public static int LEFT_SLAVE_DRIVETRAIN_ADDRESS   = 3;
+    public static int RIGHT_SLAVE_DRIVETRAIN_ADDRESS  = 4;
+    public static int TURRET_ADDRESS                  = 5;
+    public static int FIRST_SHOOTER_ADDRESS           = 6;
+    public static int SECOND_SHOOTER_ADDRESS          = 7;
+    
+    // PWM motor controllers
+    public static int INTAKER_ADDRESS                 = 0;
+    public static int FIRST_INDEXER_ADDRESS           = 1;
+    public static int SECOND_INDEXER_ADDRESS          = 2;
+    public static int THIRD_INDEXER_ADDRESS           = 3;
+    
+    // Other constants
+    public static double DRIVETRAIN_DEADBAND          = 0.26;
+    public static double TURRET_SPEED                 = 0.25;
+    
 
-
-public static void init() {
+    public static void init() {
 
     }
 }
