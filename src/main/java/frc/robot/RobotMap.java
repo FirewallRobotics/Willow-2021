080770bc48b3e29d77be1ed43f919a6193c15d1d
TylerCartier106 <46423276+TylerCartier106@users.noreply.github.com>
2020-02-08 14:39:27 -0500

    Feature/shooter and turret code (#17)
    * Turret Moving
    
    * Fixed OI
    
    * Shooter Values Edited
    
    * Robot Map Put In
    
    Co-authored-by: Terry Phillips <terrydphillips@gmail.com>

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.Preferences;
+
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+
+public static void init() {
+
+    }
+}
