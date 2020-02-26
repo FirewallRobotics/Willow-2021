f8052e857c6be80ec0f7017cf3dce8f6eb5edd4a
TylerCartier106 <46423276+TylerCartier106@users.noreply.github.com>
2020-02-25 21:02:08 -0500

    PID Loop put in (#61)
    * PID Loop put in
    
    * Pid Loop error fixed
    
    *  j

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -66,6 +66,24 @@ public class RobotMap {
     public static double EXTEND_ARMS_TIME             = 3.0;
     public static double AUTO_DRIVE_FORWARD_SPEED     = 0.30;
 
+    //Which PID slot to pull gains from. Starting 2018, you can choose from
+    //0,1,2 or 3. Only the first two (0,1) are visible in web-based configuration.
+    public static final int kSlotIdx                  = 0;
+    //Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
+    //now we just want the primary one.
+    public static final int kPIDLoopIdx               = 0;
+     //Set to zero to skip waiting for confirmation, set to nonzero to wait and
+     //report to DS if action fails.
+    public static final int kTimeoutMs                = 30;
+
+    //Add this after the last lines in RobotMap.java:
+    public static final double kP                     = 0.25;
+    public static final double kI                     = 0.001;
+    public static final double kD                     = 20;
+    public static final double kF                     = 1023.0/7200.0;
+    public final int kIzone                           = 300;
+    public final double kPeakOutput                   = 1.00;
+        
     public static void init() {
 
     }
